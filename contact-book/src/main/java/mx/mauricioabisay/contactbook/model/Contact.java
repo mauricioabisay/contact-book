ae6eb62ba9d314a8e179bfe0129f10706a4a8193
mauricioabisay <mauricioabisay.lopez@gmail.com>
2021-12-06 07:23:20 -0600

    Add Contact Hibernate mapping

---
+++ contact-book/src/main/java/mx/mauricioabisay/contactbook/model/Contact.java
@@ -0,0 +1,47 @@
+package mx.mauricioabisay.contactbook.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column
+	private String name;
+	@Column(name = "nick_name")
+	private String nickName;
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	//Getters and setters
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
